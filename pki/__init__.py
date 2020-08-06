6a88fb309c10d73fefed3153e67cb4ba64252e33
yan <wyvernnot@gmail.com>
2020-08-06 13:27:28 +0800

    add docker file

--- pki/__init__.py
+++ pki/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY") or "catfish",
         BOOTSTRAP_SERVE_LOCAL=True,  # 默认会使用 CDN 资源
-        MONGODB_URL=os.environ.get("SECRET_KEY") or "mongodb://localhost:27017"
+        MONGODB_URL=os.environ.get("MONGODB_URL") or "mongodb://localhost:27017"
     )
 
     connect("default", host=app.config.get("MONGODB_URL"))
